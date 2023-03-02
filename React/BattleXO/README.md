3ca5970ca3598558e834469585a6bc58bb3599cf
KariNurm <karinurm@gmail.com>
2023-03-02 11:37:32 +0200

    add game winning simple win conditions and some css.

--- React/BattleXO/README.md
+++ React/BattleXO/README.md
@@ -2,4 +2,6 @@
 
 ## game is made utilizing stackblitz.com as a developement environment.
 
+*** Not meant for big screens :)
 
+*** win conditions works for simple solutions, but needs to be tweeked/redone.
--- React/BattleXO/src/App.js
+++ React/BattleXO/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { useState } from 'react';
 import './style.css';
 import Board from './Board';
-import NewGame from './NewGame';
 
 export default function App() {
   const [currentGame, setCurrentGame] = useState([]);
   const [isPlayerX, setPlayerX] = useState(true);
   const [newGame, setNewGame] = useState(true);
-  const [winner, setWinner] = useState('');
+  const [winner, setWinner] = useState('X');
 
+  // handle player click on the field
   const playerClick = (id) => {
     const player = isPlayerX ? 'X' : 'O';
     let row;
@@ -46,33 +46,62 @@ export default function App() {
     currentGame[row] = newRow;
     setCurrentGame(currentGame);
     setPlayerX(!isPlayerX);
+    checkForWin();
   };
+
+  // click on new game
 
   const handleClick = () => {
     setNewGame(!newGame);
     setWinner('');
     setCurrentGame([
-      [
-        [1, ''],
-        [2, ''],
-        [3, ''],
-      ],
-      [
-        [4, ''],
-        [5, ''],
-        [6, ''],
-      ],
-      [
-        [7, ''],
-        [8, ''],
-        [9, ''],
-      ],
+      [[1, ''],[2, ''],[3, '']],  // CG [0] [1] [2]
+      [[4, ''],[5, ''],[6, '']],  // CG [3] [4] [5]
+      [[7, ''],[8, ''],[9, '']],  // CG [6] [7] [8]
     ]);
   };
+
+  // check for winning condition
+
+  const checkForWin = () => {
+    let val;
+    if(isPlayerX) { val = "X"} else { val = "O"};
+    console.log(val, "sisään")
+    console.log(currentGame.flat(),"flat")
+    const statusMap = currentGame.flat().map((ele) => {
+      if(ele[1] === val) {
+        return val
+      } else {
+        return ''
+      }
+    })
+    console.log(statusMap)
+    const winConditions = [
+      [val,'','',val,'','',val,'',''], 
+      ['',val,'','',val,'','',val,''],  
+      ['','',val,'','',val,'','',val],  
+      [val,val,val,'','','','','',''], 
+      ['','','',val,val,val,'','',''],
+      ['','','','','','',val,val,val],
+      [val,'','','',val,'','','',val],
+      ['','',val,'',val,'',val,'','']
+    ]
+    
+    winConditions.forEach(ele => {
+      if(ele.toString() === statusMap.toString()){
+        setWinner(val);
+        setNewGame(true);
+      } 
+    })
+
+  }
 
   return (
     <div className="main">
       <h1>Battle XO</h1>
+      {winner === '' ? <></>
+                     : <h1 className="winner">Winner is {winner}</h1>      
+    }
       {newGame ? (
         <div className="newGame">
           <h2>
